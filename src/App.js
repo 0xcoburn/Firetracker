8945d09e1e1843251d8ba93d485427a9088a6fbf
Matthew Coburn <mattcoburn24@gmail.com>
2020-12-09 13:29:25 -0800

    added hook to fetch data from the nasa api, then sets EventData, and Loading states. Created a loading component to display when loading is true

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
+import Loader from "./comps/Loader";
+
 import Map from "./comps/Map";
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+    const fetchEventsHandler = async () => {
+      setloading(true);
+      const res = await fetch(
+        "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+      );
+
+      const { events } = await res.json();
+
+      setEventData(events);
+      setloading(false);
+    };
+
+    fetchEventsHandler();
+  }, []);
+
   return (
     <div className="App">
-      <Map />
+      {!loading ? <Map eventData={eventData} /> : <Loader />}
     </div>
   );
 }
